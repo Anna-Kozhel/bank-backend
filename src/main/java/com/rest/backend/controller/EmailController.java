7a7e42567cdc678274b2f6c43bedc55ddab1e54a
Anna_Kozhel <anna.kozhe.pm.2020@lpnu.ua>
2023-06-07 00:05:10 +0300

    commit

--- src/main/java/com/rest/backend/controller/EmailController.java
+++ src/main/java/com/rest/backend/controller/EmailController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://bank-frontend.herokuapp.com"})
 @RestController
 public class EmailController {
 
