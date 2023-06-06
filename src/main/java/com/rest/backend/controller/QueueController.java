46e9c9650449023699e494897f0a2b885070f718
Anna_Kozhel <anna.kozhe.pm.2020@lpnu.ua>
2023-06-07 00:08:47 +0300

    commit 2

--- src/main/java/com/rest/backend/controller/QueueController.java
+++ src/main/java/com/rest/backend/controller/QueueController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://bank-frontend.herokuapp.com"})
 @RestController
 public class QueueController {
     private final QueueHardcodedService queueHardcodedService;
